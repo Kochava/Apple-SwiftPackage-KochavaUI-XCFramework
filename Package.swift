53e4bc89f254006c523fd22f7d51f036d73dd43d
John Bushnell <john.jas@icloud.com>
2024-01-30 13:04:04 -0800

    Updated package file.

--- Package.swift
+++ Package.swift
@@ -29,15 +29,14 @@ let package = Package(
     dependencies:
     [
         // Dependencies declare other packages that this package depends on.
-        .package(
-            name: "KochavaNetworking",
-            path: "./../Apple-SwiftPackage-KochavaNetworking-XCFramework"
-        ),
 //        .package(
 //            name: "KochavaNetworking",
-//            url: "https://github.com/Kochava/Apple-SwiftPackage-KochavaNetworking-XCFramework",
-//            from: "8.0.0"
-//        ),
+//            path: "./../Apple-SwiftPackage-KochavaNetworking-XCFramework"
+//        )
+        .package(
+            url: "https://github.com/Kochava/Apple-SwiftPackage-KochavaNetworking-XCFramework",
+            from: "8.0.0-rc1"
+        )
     ],
     targets:
     [
@@ -51,9 +50,12 @@ let package = Package(
             name: "KochavaUITests",
             dependencies:
             [
-                "KochavaNetworking",
+                .product(
+                    name: "KochavaNetworking",
+                    package: "Apple-SwiftPackage-KochavaNetworking-XCFramework"
+                ),
                 "KochavaUI"
             ]
-        ),
+        )
     ]
 )
